04f423d99048eb2000e677a13851dc538ec02e03
David C <211112742@students.mikroskil.ac.id>
2023-11-16 08:13:29 +0700

    Update tugas Mini Project
    menambahkan fitur dan halaman

--- Mini Project/praktikum/david_c_mini/lib/pages/home_page.dart
+++ Mini Project/praktikum/david_c_mini/lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
