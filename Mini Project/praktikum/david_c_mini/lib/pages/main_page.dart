e42af918dbb93bbef02c00a83f16bec4dec98481
David C <211112742@students.mikroskil.ac.id>
2023-11-17 21:35:48 +0700

    Update tugas Mini Project
    menambahkan fitur dan halaman

--- Mini Project/praktikum/david_c_mini/lib/pages/main_page.dart
+++ Mini Project/praktikum/david_c_mini/lib/pages/main_page.dart
@@ -1,8 +1,10 @@
 import 'package:calendar_appbar/calendar_appbar.dart';
+import 'package:david_c_mini/pages/category_page.dart';
 import 'package:david_c_mini/pages/home_page.dart';
 import 'package:flutter/material.dart';
 // import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -12,33 +14,62 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final List<Widget> _children = [HomePage(), CategoryPage()];
+  int currentIndex = 0;
+
+  void onTapTapped(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CalendarAppBar(
-        accent: Colors.green,
-        backButton: false,
-        locale: 'id',
-        onDateChanged: (value) => print(value),
-        firstDate: DateTime.now().subtract(Duration(days: 140)),
-        lastDate: DateTime.now(),
+      appBar: (currentIndex == 0)
+          ? CalendarAppBar(
+              accent: Colors.green,
+              backButton: false,
+              locale: 'en',
+              onDateChanged: (value) => print(value),
+              firstDate: DateTime.now().subtract(Duration(days: 140)),
+              lastDate: DateTime.now(),
+            )
+          : PreferredSize(
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 36, horizontal: 16),
+                  child: Text('Categories', style: GoogleFonts.montserrat(fontSize: 20)),
+                ),
+              ),
+              preferredSize: Size.fromHeight(100)),
+      floatingActionButton: Visibility(
+        visible: (currentIndex == 0) ? true : false,
+        child: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.green,
+          child: Icon(Icons.add),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.green,
-        child: Icon(Icons.add),
-      ),
-      body: HomePage(),
+      body: _children[currentIndex],
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomAppBar(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            IconButton(onPressed: () {}, icon: Icon(Icons.home)),
+            IconButton(
+                onPressed: () {
+                  onTapTapped(0);
+                },
+                icon: Icon(Icons.home)),
             SizedBox(
               width: 20,
             ),
-            IconButton(onPressed: () {}, icon: Icon(Icons.list)),
+            IconButton(
+                onPressed: () {
+                  onTapTapped(1);
+                },
+                icon: Icon(Icons.list)),
           ],
         ),
       ),
